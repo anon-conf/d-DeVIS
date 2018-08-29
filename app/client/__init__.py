3787ce0270cd93948deefd180bffdbda319ffd28
Tahsin Al Sayeed <tahsinalsayeed@gmail.com>
2018-08-30 00:13:41 +0600

    renamed dist folder to bundle

--- app/client/__init__.py
+++ app/client/__init__.py
@@ -6,8 +6,8 @@ from flask import Blueprint, render_template
 client_bp = Blueprint('client_app', __name__,
                       url_prefix='',
                       static_url_path='',
-                      static_folder='./vue_app/dist',
-                      template_folder='./vue_app/dist',
+                      static_folder='./vue_app/bundle',
+                      template_folder='./vue_app/bundle',
                       )
 
 @client_bp.route('/')
